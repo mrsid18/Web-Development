3b39d3d5023276313440fa98f9977fcd4a422a1d
mrsid18 <siddharthamehta18@gmail.com>
2021-01-04 15:41:03 +0530

    Added some styling and removed unused components

--- Notes/index.php
+++ Notes/index.php
@@ -54,20 +54,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
   <link rel="stylesheet" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
   <title>Notes</title>
+  <style>
+  body{
+    font-family: 'Raleway', sans-serif;
+  }
+  h1{
+    font-weight: 800;
+    font-size: 5rem;
+  }
+  h2{
+    font-weight: 300;
+  }
+  </style>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Notes</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </div>
-    </div>
-  </nav>
+
 
   <?php
   if ($insert) {
@@ -139,27 +145,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </div>
   </div>
 
-  <div class="container">
-    <h1>Add a new note</h1>
+  <div class="container p-5 mt-3">
+    <h1>Notes</h1>
     <form action="index.php" method="POST">
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Title</label>
+        <label for="exampleFormControlInput1" class="form-label"><h5>Title</h5></label>
         <input type="text" class="form-control" id="title" name="title" />
       </div>
       <div class="mb-3">
-        <label for="description" class="form-label">Description</label>
+        <label for="description" class="form-label"><h5>Description</h5></label>
         <textarea class="form-control" id="description" rows="3" name="description"></textarea>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">Add Note</button>
     </form>
     <br>
     <table class="table" id="myTable">
       <thead>
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">Title</th>
-          <th scope="col">Description</th>
-          <th scope="col">Actions</th>
+          <th scope="col" style="width: 5%;">#</th>
+          <th scope="col" style="width: 25%;">Title</th>
+          <th scope="col" style="width: 53%;">Description</th>
+          <th scope="col" style="width: 17%;">Actions</th>
         </tr>
       </thead>
       <tbody>
@@ -171,7 +177,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
           echo '<tr>
             <th scope="row">' . $index++ . '</th>
             <td>' . $num['title'] . '</td>
-            <td>' . $num['description'] . '</td>
+            <td style="white-space: pre">' . $num['description'] . '</td>
             <td><button class="edit btn-primary btn-sm mx-3" id="' . $num['srno'] . '" >Edit</button><button class="delete btn-primary btn-sm" id=d"' . $num['srno'] . '">Delete</button></td>
           </tr>';
         }
