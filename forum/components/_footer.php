f746b83b446edb3515059bf59fb652600e0f516a
mrsid18 <siddharthamehta18@gmail.com>
2021-01-04 15:42:48 +0530

    Renamed files

--- forum/components/_footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer mt-auto py-3 bg-light">
-  <div class="container">
-    <p class="text-center"><span class="text-muted">Copyright &copy;2020-21 Programming Wiz</span></p>
-  </div>
-</footer>
